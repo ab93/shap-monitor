fix shap calculation
Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

tests/test_monitor.py
@@ -1,13 +1,14 @@
+import numpy as np
 import pytest
+import shap
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
 from shapmonitor import SHAPMonitor
 from shapmonitor.types import ExplainerLike
 
 
 @pytest.fixture
 def model():
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.datasets import make_regression
-
     X, y = make_regression(n_samples=100, n_features=5, random_state=42)
     model = RandomForestRegressor()
     model.fit(X, y)
@@ -16,8 +17,6 @@ def model():
 
 @pytest.fixture
 def explainer(model) -> ExplainerLike:
-    import shap
-
     return shap.Explainer(model)
 
 
@@ -31,3 +30,7 @@ def test_monitor(monitor):
     assert monitor.sample_rate == 0.2
     assert monitor.data_dir.exists()
     print(monitor.explainer)
+
+    shap_values = monitor.compute_shap(np.array([[0.5] * 5]))
+    print(shap_values)
+    shap.plots.beeswarm(shap_values)